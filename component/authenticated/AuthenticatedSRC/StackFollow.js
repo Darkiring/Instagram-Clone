1a468eabc83be1ce88f32f23b9846d1106c87643
Abraham Rosa Vargas <abrahamarv@hotmail.es>
2018-06-22 15:15:59 -0400

    Video 29

--- component/authenticated/AuthenticatedSRC/StackFollow.js
+++ component/authenticated/AuthenticatedSRC/StackFollow.js
@@ -7,6 +7,9 @@ import Comentarios from './tools/Follow';
 const StackFollow = StackNavigator({
   TabFollow: {
     screen: TabFollow,
+    navigationOptions: {
+      header: null,
+    },
   },
   Autor: {
     screen: Autor,
