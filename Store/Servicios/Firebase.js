141bedb0c3857484e19c8a77f4a69fe7850b0d99
Abraham Rosa Vargas <abrahamarv@hotmail.es>
2018-07-11 16:43:08 -0400

    Video 40, Integrated Firebase

---
+++ Store/Servicios/Firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const config = {
+  apiKey: 'AIzaSyBMbtbj5T_AF235ePQE9uWQ74m5YHxO8t4',
+  authDomain: 'instagram-clone-5f2e3.firebaseapp.com',
+  databaseURL: 'https://instagram-clone-5f2e3.firebaseio.com',
+  projectId: 'instagram-clone-5f2e3',
+  storageBucket: 'instagram-clone-5f2e3.appspot.com',
+  messagingSenderId: '760334358504',
+};
+firebase.initializeApp(config);
